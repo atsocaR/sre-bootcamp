b540e32197184d1460eb52a1922097059d69186c
Edgar <edgar.acosta@ext.cemex.com>
2023-02-12 20:07:24 -0600

    second commit

--- auth_api/python/tests.py
+++ auth_api/python/tests.py
@@ -9,10 +9,14 @@ class TestStringMethods(unittest.TestCase):
         self.validate = Restricted()
 
     def test_generate_token(self):
-        self.assertEqual('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI', self.convert.generate_token('admin', 'secret'))
+        self.assertEqual(
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI',
+            self.convert.generate_token('admin', 'secret'))
 
     def test_access_data(self):
-        self.assertEqual('You are under protected data', self.validate.access_data('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI'))
+        self.assertEqual('You are under protected data', self.validate.access_data(
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI'))
+
 
 if __name__ == '__main__':
     unittest.main()
